a913703b428df49120db8a99c564e5ffbdecfb63
khairnaralalit07 <khairnarlalit845@gmail.com>
2025-10-07 14:23:54 +0530

    .

--- src/main/java/com/SpringLearning25Application.java
+++ src/main/java/com/SpringLearning25Application.java
@@ -2,12 +2,20 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringLearning25Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearning25Application.class, args);
+	}
+	
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bean.UserBean;
+import com.dao.UserDao;
 
 @Controller
 public class UserController {
@@ -13,6 +20,14 @@ public class UserController {
 //		return "Signup";
 //	}
 	
+	@Autowired
+	PasswordEncoder enocder;
+	
+	@Autowired
+	JdbcTemplate stmt;
+	
+	@Autowired
+	UserDao userDao;
 	
 	@PostMapping("/saveuser")
 	public String savedUser(String firstName, String lastName, String email, String password, String gender, String city,Model model) {
@@ -63,6 +78,25 @@ public class UserController {
 		}
 		
 		else {
+			
+//			stmt.update("INSERT INTO users (firstName, lastName, email, password) values (?, ?, ?, ?)",firstName, lastName, email, password);
+			
+			
+//			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			
+			String encrytedPassword = enocder.encode(password);
+			
+			System.out.println("encrypted Password => " + encrytedPassword);
+			
+			UserBean userBean = new UserBean();
+			
+			userBean.setFirstName(firstName);
+			userBean.setLastName(lastName);
+			userBean.setEmail(email);
+			userBean.setPassword(encrytedPassword);
+			
+			userDao.addUser(userBean);
+			
 			return "Login";
 		}
 		
