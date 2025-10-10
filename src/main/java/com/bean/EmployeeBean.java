cfce8d6e0d0a53ca4df0dfa0c77a6aac845aec3b
khairnaralalit07 <khairnarlalit845@gmail.com>
2025-10-10 12:33:24 +0530

    .

---
+++ src/main/java/com/bean/EmployeeBean.java
@@ -0,0 +1,50 @@
+package com.bean;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class EmployeeBean {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private MultipartFile profilePic;
+	private String profilePicUrl;
+	public String getProfilePicUrl() {
+		return profilePicUrl;
+	}
+	public void setProfilePicUrl(String profilePicUrl) {
+		this.profilePicUrl = profilePicUrl;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public MultipartFile getProfilePic() {
+		return profilePic;
+	}
+	public void setProfilePic(MultipartFile profilePic) {
+		this.profilePic = profilePic;
+	}
+	
+}
